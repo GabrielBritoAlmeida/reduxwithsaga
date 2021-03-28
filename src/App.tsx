9fe5baaa43c27ac16cc8908d4e213095c19175ed
Gabriel g3 <gabrielfranccelino@gmail.com>
2021-03-28 13:44:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Provider } from 'react-redux'
+import Catalog from 'components/Catalog'
+import store from './store'
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/components/Catalog.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { api } from 'services/api'
+import { addProductToCart } from 'store/modules/cart/action'
+import { IProduct } from 'store/modules/cart/types'
+
+const Catalog: React.FC = () => {
+  const dispatch = useDispatch()
+  const [catalog, setCatalog] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    api.get('products').then((response) => {
+      setCatalog(response.data)
+    })
+  }, [])
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [])
+
+  return (
+    <div>
+      <h1>Catalog</h1>
+
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {' - '}
+          <span>{product.price}</span> {'  '}
+          <button type="button" onClick={() => handleAddProductToCart(product)}>
+            Comprar
+          </button>
+        </article>
+      ))}
+    </div>
+  )
+}
+
+export default Catalog
